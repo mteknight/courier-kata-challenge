477360edd2390210293550a4de069f77e01508c6
Alexandre Fernandes <mteknight@hotmail.com>
2020-06-13 17:24:44 +0000

    created basic project for kata

---
+++ Courier Kata/CourierKata.Services.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+
+using Xunit;
+
+namespace CourierKata.Services.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
